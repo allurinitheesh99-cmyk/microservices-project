7e9be6ffd397db4eacb57323554d20fdaa0bfbb7
allurinithesh99-cmyk <allurinitheesh99@gmail.com>
2026-05-22 11:08:22 -0400

    Fixed CORS and completed dashboard CRUD features

--- ecommerce-frontend/src/pages/Dashboard.js
+++ ecommerce-frontend/src/pages/Dashboard.js
@@ -1,5 +1,8 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import {
+    useEffect,
+    useState
+}
+    from "react";
 
 import {
     useDispatch,
@@ -13,10 +16,9 @@ import {
     updateProduct
 }
     from "../features/productSlice";
+
 import Navbar
     from "../components/Navbar";
-
-
 
 function Dashboard() {
 
@@ -26,6 +28,7 @@ function Dashboard() {
     const [search,
         setSearch] =
         useState("");
+
     const {
         products,
         loading,
@@ -33,6 +36,13 @@ function Dashboard() {
     } = useSelector(
         (state) => state.product
     );
+
+    useEffect(() => {
+
+        dispatch(fetchProducts());
+
+    }, [dispatch]);
+
     const filteredProducts =
         products.filter(product =>
 
@@ -42,12 +52,6 @@ function Dashboard() {
                     search.toLowerCase()
                 )
         );
-
-    useEffect(() => {
-
-        dispatch(fetchProducts());
-
-    }, [dispatch]);
 
     if (loading) {
 
@@ -67,100 +71,121 @@ function Dashboard() {
 
             <div className="container mt-5">
 
-                <h2>Product Dashboard</h2>
+                <h2>
+                    Product Dashboard
+                </h2>
+
                 <input
                     type="text"
                     placeholder="Search Product"
-                    className="form-control mb-4"
+                    className=
+                        "form-control mb-4"
+
                     value={search}
+
                     onChange={(e) =>
-                        setSearch(e.target.value)}
+                        setSearch(
+                            e.target.value
+                        )
+                    }
                 />
 
                 {
-                    filteredProducts.map(product =>(
+                    filteredProducts.map(
+                        product => (
 
-                        <div
-                            key={product.id}
-                            className="card p-3 mb-3"
-                        >
-
-                            <h4>
-                                {product.name}
-                            </h4>
-
-                            <p>
-                                Price:
-                                ${product.price}
-                            </p>
-
-                            <p>
-                                Stock:
-                                {product.stock}
-                            </p>
-
-                            <button
-                                className="btn btn-danger"
-                                onClick={() =>
-                                    dispatch(
-                                        deleteProduct(product.id)
-                                    )
-                                }
-                            >
-
-                                Delete
-
-                            </button>
-
-                            <button
+                            <div
+                                key={product.id}
                                 className=
-                                    "btn btn-warning ms-2"
-                                onClick={() => {
-
-                                    const updatedName =
-                                        prompt(
-                                            "Enter Product Name",
-                                            product.name
-                                        );
-
-                                    const updatedPrice =
-                                        prompt(
-                                            "Enter Price",
-                                            product.price
-                                        );
-
-                                    const updatedStock =
-                                        prompt(
-                                            "Enter Stock",
-                                            product.stock
-                                        );
-
-                                    const updatedProduct = {
-
-                                        name: updatedName,
-                                        price: updatedPrice,
-                                        stock: updatedStock
-                                    };
-
-                                    dispatch(
-                                        updateProduct({
-
-                                            id: product.id,
-
-                                            product:
-                                            updatedProduct
-                                        })
-                                    );
-                                }}
+                                    "card p-3 mb-3"
                             >
 
-                                Edit
+                                <h4>
+                                    {product.name}
+                                </h4>
 
-                            </button>
+                                <p>
+                                    Price:
+                                    ${product.price}
+                                </p>
 
-                        </div>
+                                <p>
+                                    Stock:
+                                    {product.stock}
+                                </p>
 
-                    ))
+                                <button
+                                    className=
+                                        "btn btn-danger"
+
+                                    onClick={() =>
+                                        dispatch(
+                                            deleteProduct(
+                                                product.id
+                                            )
+                                        )
+                                    }
+                                >
+
+                                    Delete
+
+                                </button>
+
+                                <button
+                                    className=
+                                        "btn btn-warning ms-2"
+
+                                    onClick={() => {
+
+                                        const updatedName =
+                                            prompt(
+                                                "Enter Product Name",
+                                                product.name
+                                            );
+
+                                        const updatedPrice =
+                                            prompt(
+                                                "Enter Price",
+                                                product.price
+                                            );
+
+                                        const updatedStock =
+                                            prompt(
+                                                "Enter Stock",
+                                                product.stock
+                                            );
+
+                                        const updatedProduct = {
+
+                                            name:
+                                            updatedName,
+
+                                            price:
+                                            updatedPrice,
+
+                                            stock:
+                                            updatedStock
+                                        };
+
+                                        dispatch(
+                                            updateProduct({
+
+                                                id:
+                                                product.id,
+
+                                                product:
+                                                updatedProduct
+                                            })
+                                        );
+                                    }}
+                                >
+
+                                    Edit
+
+                                </button>
+
+                            </div>
+                        ))
                 }
 
             </div>
